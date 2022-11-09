4fe4b24da5531ac8396e8ed8985315d2a438b0df
Suriya0707 <113662732+Suriya0707@users.noreply.github.com>
2022-11-09 16:49:51 +0530

    Update python code .py

--- DEVELOP A PYTHON SCRIPT TO PUBLISH AND SUBSCRIBE TO IBM IOT PLATFORM/Develop a Python Code/python code .py
+++ DEVELOP A PYTHON SCRIPT TO PUBLISH AND SUBSCRIBE TO IBM IOT PLATFORM/Develop a Python Code/python code .py
@@ -1,3 +1,4 @@
+#Develop a python script to send the random sensor data of Hazardous gas levels, temperature, humidity, pressure, etc.
 import random
 print("hum: ",str(random.randint(0,100)))
 print("temp: ",str(random.randint(0,100)))
