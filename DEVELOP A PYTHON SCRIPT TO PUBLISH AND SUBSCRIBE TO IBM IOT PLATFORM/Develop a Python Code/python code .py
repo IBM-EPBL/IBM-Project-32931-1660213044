8948a985751cd27b269682c9d87ea552b34c1bef
Suriya0707 <113662732+Suriya0707@users.noreply.github.com>
2022-11-09 16:48:55 +0530

    Create python code .py

---
+++ DEVELOP A PYTHON SCRIPT TO PUBLISH AND SUBSCRIBE TO IBM IOT PLATFORM/Develop a Python Code/python code .py
@@ -0,0 +1,5 @@
+import random
+print("hum: ",str(random.randint(0,100)))
+print("temp: ",str(random.randint(0,100)))
+print("gas: ",str(random.randint(0,100)))
+print("pressure: ",str(random.randint(0,100)))
